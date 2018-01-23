a16d5f48a3c850f595ca72ed02722037bf87c304
Nejat-Ali1 <35277799+Nejat-Ali1@users.noreply.github.com>
2018-01-23 13:55:16 -0800

    Add files via upload

---
+++ quadScript.js
@@ -0,0 +1,25 @@
+//Javascript for drawing and calculating quadratics 2017
+var a, b, c, context, w, h, k=10;
+
+function init() {
+  canvas= document.getElementById("mycanvas");
+  context = canvas.getContext("2d");
+  w = canvas.width = 600;
+  h = canvas.height = 400;
+  console.log('canvas is loaded into context',w);
+}  // close init
+
+function QF() {
+  // getting values to do quadratic formula
+  a = $("#quadA").val();
+  b = $("#linB").val();
+  c = $("#constant").val();
+  console.log(a,b,c);
+}  // close QF
+
+function results() {
+  // finding vertext and displaying symline and yint results
+  vX = -(b*1)/(2*a);
+  vY = a*Math.pow(vX,2)+b*vX+c*1;
+  $(".vertex").text("Vertex is at (" + vX+","+vY+")");
+}  // close results()
