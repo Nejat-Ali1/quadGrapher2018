f1b57e8223cf657c689f7689eec6f6c4fe818b24
Nejat-Ali1 <35277799+Nejat-Ali1@users.noreply.github.com>
2018-02-27 13:33:21 -0800

    2/27/18
    yup! tuesday man

--- quadScript.js
+++ quadScript.js
@@ -7,6 +7,10 @@ function init() {
   w = canvas.width = 600;
   h = canvas.height = 400;
   console.log('canvas is loaded into context',w);
+  canvasOffset = canvas.getBoundingClientRect();
+  offsetX = Math.round(canvasOffset.left),
+  offsetY = Math.round(canvasOffset.top); 
+  canvas.addEventListener("mousemove", doMouseMove, false);
   graphpaper();
   QF();
 
@@ -18,17 +22,81 @@ function QF() {
   a = $("#quadA").val();
   b = $("#linB").val();
   c = $("#constant").val();
+  d=b**2-4*a*c;
+  if (d<0) {
+    $("#solution1").text("No x-intercepts"); 
+    $("#solution2").text("No x-intercepts"); 
+  }
+  else { 
+
   x1 = (-b +Math.sqrt(b**2 - 4*a * c)) / (2*a);
   x1= Math.round(x1*100)/100;
   x2 = (-b -Math.sqrt(b**2 - 4*a * c)) / (2*a);
   x2= Math.round(x2*100)/100;
   $("#solution1").text("X Interceot is at "+x1);
   $("#solution2").text("X Interceot is at "+x2);
+}
+
+ ctx.beginPath();
+ ctx.fillStyle= "black";
+  ctx.arc(w/2+x1*k,h/2,10,0,6.28);
+  ctx.fill();
+
+   ctx.beginPath();
+  ctx.arc(w/2+x2*k,h/2,10,0,6.28);
+  ctx.fill();
     console.log(a,b,c);
     results(); 
     graphpaper(); 
     graphQuad();
+
 }  // close QF
+function zoomIn() {
+k=k+3; 
+resetCanvas(); 
+}
+
+function zoomOut() {
+k=k-3;
+if (k<3){ k =
+ 3}
+resetCanvas();
+}
+
+function resetCanvas() {
+  init();
+  graphQuad();
+  results();
+  QF();
+}
+
+
+
+
+
+
+
+
+
+
+function doMouseMove(event) {
+    // always know where ther mouse is locate
+  resetCanvas();
+  mouseX = event.clientX-offsetX;
+  mouseY = event.clientY-offsetY;
+  pointX = (mouseX-w/2)/k;
+  pointY = a*Math.pow(pointX,2)+b*pointX+c*1;
+  pointX =  pointX.toFixed(2);
+  pointY =  pointY.toFixed(2);
+  //console.log(mouseX,mouseY, pointX, pointY, offsetY, offsetX);
+  ctx.fillStyle= "purple";
+  ctx.beginPath();
+  ctx.arc(mouseX,h/2-pointY*k,5,0,2*Math.PI);
+  ctx.fill(); 
+  $("#point").text("Point on the curve: ("+pointX+","+pointY+")");
+}  // end doMouseMove
+
+
 
 function results() {
   // finding vertext and displaying symline and yint results
@@ -36,18 +104,21 @@ function results() {
   vY = a*Math.pow(vX,2)+b*vX+c*1;
   $("#vertex").text("Vertex is at (" + vX+","+vY+")");
   ctx.beginPath();
+  ctx.fillStyle = "red";
   ctx.arc(w/2+vX*k,h/2-vY*k,10,0,6.28);
   ctx.fill();
   $("#y-int").text(" Y intercept is at (0,"+ c +")");
-  ctx.fillStyle="green";
+  ctx.fillStyle="gray";
   ctx.beginPath();
   ctx.arc(w/2,h/2-c*k,10,0,6.28);
   ctx.fill();
   Xcp = 2*vX; 
   $("#cp").text ("corr. Pt is at ("+Xcp+","+c+")"); 
+  ctx.fillStyle= "blue";
 ctx.beginPath();
-  ctx.arc(w,h/2-c*k,10,0,6.28);
+  ctx.arc(w/2+2*vX*k,h/2-c*k,10,0,6.28);
   ctx.fill();
+    $("#sym").text ("symmetry line is at x= "+vX);
 
 }  // close results()
 
@@ -108,8 +179,7 @@ function graphQuad () {
     y = c*1+b*x+a*Math.pow(x,2);
     nx =  (w/2 -(i+1))/k ;
     ny =  c*1+b*nx+a*Math.pow(nx,2);
-    console.log(x,y,nx,ny); 
-
+    
     ctx.beginPath();
     ctx.lineWidth = 2;
     ctx.strokeStyle = "red";
@@ -117,4 +187,19 @@ function graphQuad () {
     ctx.lineTo(w/2+nx*k,h/2-ny*k);
     ctx.stroke();
   }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
